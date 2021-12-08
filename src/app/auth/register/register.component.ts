8e8d0b4038dce3e4fa2ce5203da8878dd98c14f4
yfsanchez <yfsanchez@gmail.com>
2021-12-08 16:38:37 -0300

    formulario y valores estaticos
    formulario y valores estaticos

--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: [ './register.component.css' ]
 })
-export class RegisterComponent implements OnInit {
+export class RegisterComponent {
 
-  constructor() { }
+  public registerForm = this.fb.group({
+    nombre : ['Yadroy', Validators.required],
+    email : ['yfsancheztest100@gmail.com', Validators.required],
+    password : ['123456', Validators.required],
+    password2 : ['123456', Validators.required],
+    terminos : [false, Validators.required],
+  });
+  constructor(private fb: FormBuilder) { }
 
-  ngOnInit(): void {
+  
+  crearUsuario(){
+    console.log(this.registerForm.value);
+    
   }
 
 }
