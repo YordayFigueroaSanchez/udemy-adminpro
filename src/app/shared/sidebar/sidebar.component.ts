68eb00cb4c8f690ece9297e2c2eed4035e483040
yfsanchez <yfsanchez@gmail.com>
2021-12-10 11:54:31 -0300

    agregando img en otros espacios
    agregando img en otros espacios

--- src/app/shared/sidebar/sidebar.component.ts
+++ src/app/shared/sidebar/sidebar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { UsuarioService } from 'src/app/services/usuario.service';
 import { SidebarService } from '../../services/sidebar.service';
 
 @Component({
@@ -10,10 +11,13 @@ import { SidebarService } from '../../services/sidebar.service';
 export class SidebarComponent implements OnInit {
 
   menuItems: any[];
+  public imgUrl = '';
 
-  constructor( private sidebarService: SidebarService ) {
+  constructor( private sidebarService: SidebarService,
+                private usurioService: UsuarioService ) {
     this.menuItems = sidebarService.menu;
-    console.log(this.menuItems)
+    console.log(this.menuItems);
+    this.imgUrl = usurioService.usuario.imageUrl;
   }
 
   ngOnInit(): void {
